3ef501d6ba5d4121881f3036a9414250026b7bf6
Lina-Blaberg-NTI-Johanneberg <Lina.blaberg@elev.ga.ntig.se>
2022-03-08 12:49:12 +0100

    åttonde mars stylea

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("lina är bäst")
+let i = 1
+
+setInterval(byta_bild, 5000)
+
+function byta_bild(){
+    document.querySelector('#nyhetse').innerHTML = `<img id = "nyhets_bild" class = "fade" src="img/camp_${i}.jpeg" alt="Nyheter om innebandy">`
+
+    if(i>=6){
+        i = 1
+    }else{
+        i++
+    }
+
+}
