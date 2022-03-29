25ef91ed0a6a82c5a8d9e0d0685aa4d0bed0e124
Lina-Blaberg-NTI-Johanneberg <Lina.blaberg@elev.ga.ntig.se>
2022-03-29 11:16:30 +0200

    förmodligen sista

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ let i = 1
 setInterval(byta_bild, 5000)
 
 function byta_bild(){
-    document.querySelector('#nyhetse').innerHTML = `<img id = "nyhets_bild" class = "fade" src="img/camp_${i}.jpeg" alt="Nyheter om innebandy">`
+    document.querySelector('#nyhetse').innerHTML = `<img id = "nyhets_bild" class = "fade" src="img/innebandy_${i}.jpg" alt="bilder på olika lag i innebandyklubben">`
 
-    if(i>=6){
+    if(i>=4){
         i = 1
     }else{
         i++
